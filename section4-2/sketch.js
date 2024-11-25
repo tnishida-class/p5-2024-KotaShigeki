dc424a1d59caee76a2281bd1fc9adc7134ce35fb
KotaShigeki <souabout0330@outlook.jp>
2024-11-25 23:17:19 +0900

    lastwork

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -4,30 +4,44 @@
 
 let balls;
 
-function setup(){
+function setup() {
   createCanvas(windowWidth, windowHeight);
   balls = [];
+  setInterval(createRandomBall, 1000); // 毎秒1個ボールを生成
 }
 
-function draw(){
+function draw() {
   background(160, 192, 255);
-  for(let i = 0; i < balls.length; i++){
+  for (let i = 0; i < balls.length; i++) {
     let b = balls[i];
     ellipse(b.x, b.y, b.size);
     b.x += b.vx;
     b.y += b.vy;
+    b.size += random(-2, 2); // ボールのサイズをランダムに変更
+    b.size = constrain(b.size, 10, 100); // サイズを10～100の範囲に制限
   }
 }
 
-function mouseDragged(){
+function mouseDragged() {
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
-  if(mag(dx, dy) > 5){
+  if (mag(dx, dy) > 5) {
     const b = { x: mouseX, y: mouseY, size: random(10, 100), vx: dx, vy: dy };
     balls.push(b);
   }
 }
 
-function windowResized(){
+function createRandomBall() {
+  const b = {
+    x: random(width),
+    y: random(height),
+    size: random(10, 100),
+    vx: random(-2, 2),
+    vy: random(-2, 2),
+  };
+  balls.push(b);
+}
+
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
--- section5-3/sketch.js
+++ section5-3/sketch.js
@@ -26,7 +26,12 @@ function isLeapYear(y){
 }
 
 function daysInYear(y){
-  // BLANK[1]
+  if(isLeapYear(y)){
+    return 366;
+  }
+  else{
+    return 365;
+  }
 }
 
 function daysInMonth(y, m){
@@ -50,7 +55,13 @@ function dayOfYear(y, m, d){
 }
 
 function dayOfWeek(y, m, d){
-  // BLANK[2]
+  if (m < 3) {
+    y -= 1;
+    m += 12;
+  }
+  let k = y % 100; // 世紀内の年
+  let j = floor(y / 100); // 世紀
+  return (d + floor((13 * (m + 1)) / 5) + k + floor(k / 4) + floor(j / 4) - 2 * j) % 7;
 }
 
 function dayOfWeekAsString(dow){
